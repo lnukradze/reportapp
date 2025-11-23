47f574e0ab5b57df63c2ee8f100b69c4fed2b184
lnukradze <167809044+lnukradze@users.noreply.github.com>
2025-11-23 22:53:56 +0400

    Update server.py

--- WFS_DASHBOARD - Copy/server.py
+++ WFS_DASHBOARD - Copy/server.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 import os, json, urllib.parse
 from datetime import datetime
 import requests
@@ -179,4 +180,5 @@ def api_data():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
